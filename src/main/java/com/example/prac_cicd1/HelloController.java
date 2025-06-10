64f9b56a6cf69f79867aea32bec9b1f63533426e
victoai <victoai999@gmail.com>
2025-06-10 09:20:20 +0900

    cicd 1

---
+++ src/main/java/com/example/prac_cicd1/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.prac_cicd1;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+    @GetMapping("/hi")
+    public String hi(){
+        return "hi cicd  prac1";
+    }
+
+
+}
